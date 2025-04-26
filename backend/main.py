86bfb4939ca79ab21a656ae9babaa244cf252a84
andrewkozinski <andrewkozinski76@gmail.com>
2025-04-26 16:00:35 -0400

    Fixed get stock data route and added routes to fetch news articles related to a ticker and get a ticker logo

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import requests
 from fastapi import FastAPI
 import yfinance as yf
 
@@ -20,20 +21,50 @@ async def get_stock_data(ticker: str, period: Period):
     """
     Fetch stock data for a given ticker, period, and interval.
     """
-    valid_periods = ["1d", "5d", "1mo"]
     intervals = {
-        period.ONE_DAY: ["5m"],
-        period.FIVE_DAY: ["15m"],
-        period.ONE_MONTH: ["1d"]
+        period.ONE_DAY: "5m",
+        period.FIVE_DAY: "15m",
+        period.ONE_MONTH: "1d"
+    }
+    periods = {
+        period.ONE_DAY: "1d",
+        period.FIVE_DAY: "5d",
+        period.ONE_MONTH: "1mo"
     }
 
+    valid_periods = [period.ONE_DAY, period.FIVE_DAY, period.ONE_MONTH]
     if period not in valid_periods:
         return {"error": "Invalid period"}
 
     #get the current interval
     interval = intervals.get(period, [])
+    period_string = periods.get(period, [])
 
     stock = yf.Ticker(ticker)
-    data = stock.history(period=period, interval=interval)
+    data = stock.history(period=period_string, interval=interval)
     return data.to_dict(orient="records")
 
+#Fetch news articles for a given ticker
+@app.get("/news/{ticker}")
+async def get_news(ticker: str):
+    """
+    Fetch news articles for a given ticker.
+    """
+    stock = yf.Ticker(ticker)
+    news = stock.news
+    return news
+
+#logo.dev search and get a logo
+@app.get("/logo/{ticker}")
+async def get_logo(ticker: str):
+    """
+    Fetch logo for a given company.
+    """
+    public_token = "pk_PB5o5CpXRiSO0JcU6FzItw"
+    url = f"https://img.logo.dev/ticker/{ticker}?token={public_token}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return {"logo_url": url}
+    else:
+        return {"error": "Failed to fetch logo"}
