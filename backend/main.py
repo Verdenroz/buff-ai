7eda1bb5d4342fd31e53a995dd9e439821705646
andrewkozinski <andrewkozinski76@gmail.com>
2025-04-26 19:01:26 -0400

    tts route added which should get presigned key

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,21 @@
+import os
+
+import boto3
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import yfinance as yf
 
 from models.historical import Period
+from models.post import Post
+
+from elevenlabs import ElevenLabs
+
+from rds import RedisHandler
+from s3 import S3
 
 app = FastAPI()
+
+elevenlabs = os.getenv("ELEVENLABS_API_KEY")
 
 @app.get("/")
 async def root():
@@ -54,6 +65,23 @@ async def get_news(ticker: str):
     news = stock.news
     return news
 
+#Get company funamentals
+@app.get("/fundamentals/{ticker}")
+async def get_fundamentals(ticker: str):
+    """
+    Fetch company fundamentals for a given ticker.
+    """
+    stock = yf.Ticker(ticker)
+    fundamentals = {
+        "info": stock.info,
+        "sustainability": stock.sustainability,
+        "calendar": stock.calendar,
+        "actions": stock.actions,
+        "dividends": stock.dividends,
+        "splits": stock.splits,
+    }
+    return fundamentals
+
 #logo.dev search and get a logo
 @app.get("/logo/{ticker}")
 async def get_logo(ticker: str):
@@ -68,3 +96,39 @@ async def get_logo(ticker: str):
         return {"logo_url": url}
     else:
         return {"error": "Failed to fetch logo"}
+
+
+#Get a posts tts and create an audiofile on elevenlabs
+#Then should but put in s3 bucket
+@app.get("/tts")
+async def get_tts(key:str = Query(...)):
+    # """
+    # Get post from redis and get from s3
+    # """
+    # #client = ElevenLabs(api_key=elevenlabs)
+    # # generated_audio = client.text_to_speech.convert(
+    # #     voice_id="JBFqnCBsd6RMkjVDRZzb",
+    # #     output_format="mp3_44100_128",
+    # #     text="Test 123",
+    # #     model_id="eleven_multilingual_v2",
+    # # )
+    #
+    #
+    # #Get from redis here
+    # rds = RedisHandler()
+    #
+    # #Get the post from redis
+    # post = rds.get(f"trump_post:{key}")
+    # if not post:
+    #     return {"error": "Post not found"}
+    #
+    # #s3 key
+    # key = f"tts/trump/{post['date']}.mp3"
+
+    #Get the audio file from s3
+    s3 = S3()
+    audio_file = s3.get_presigned_url(key)
+    if not audio_file:
+        return {"error": "Audio file not found"}
+    #return presigned url
+    return {"audio_file": audio_file}
